3a455fb2478f305ca31d18cd1d19e40ac7608e83
Amanda Haldy <aehaldy@tutanota.com>
2020-04-22 20:38:54 -0400

    Creates basic file structure for Cart miplementation

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
-const isAdmin = require('./isAdmin')
 
 module.exports = router
 
 router.use('/users', require('./users'))
 router.use('/puzzles', require('./puzzles'))
+router.use('/cart', require('./carts'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
--- server/db/models/order.js
+++ server/db/models/order.js
@@ -21,4 +21,8 @@ const Order = db.define('orders', {
   }
 })
 
+Order.prototype.isInCart = function() {
+  return this.stillInCart
+}
+
 module.exports = Order
