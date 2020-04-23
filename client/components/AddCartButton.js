d9f8f95d81d72f81358e4425e70b6e6a323aae6a
Shannon <scrowley016@gmail.com>
2020-04-23 11:41:32 -0400

    pull party

--- client/components/AddCartButton.js
+++ client/components/AddCartButton.js
@@ -6,7 +6,7 @@ export default class AllPuzzles extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div className="allbuttons">
         <div>
           <button type="button">ADD TO CART</button>
         </div>
