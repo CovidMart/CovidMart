1dc8ce6cf8920aa4a56c627bf73d8434d9cb8fb1
Amanda Haldy <aehaldy@tutanota.com>
2020-04-23 16:37:59 -0400

    FEATURE: Basic guest cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,22 +1,18 @@
 import React from 'react'
-// TBD -- this is PLACEHOLDER
-// A dumb rendering component
 
 const Cart = props => {
-  //array pf puzzles should include
-  //all puzzle table data for puzzles in cart
-  //as well as quantity of each puzzle
-  const {orderArr} = props
+  const {orderArray} = props
   //handlers for add and delete will have to be passed in as well
   //If nothing is in the cart guide the user back to shopping
-  console.log('Got proprs?', orderArr)
+  console.log('Got props?', orderArray)
   return (
     <div>
       <h1>Party Carty!</h1>
       <ol>
-        {orderArr.map(item => (
+        {orderArray.map(item => (
           <li key={item.id}>
             <h4>{item.title}</h4>
+            <p>{`Qty: ${item.qty} -- Price: ${item.price} ea.`}</p>
           </li>
         ))}
       </ol>
