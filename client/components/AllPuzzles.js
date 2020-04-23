1684328de8668dea4e7a5afd264c2a8a91fe17cd
Carmen <xiawenwu22@gmail.com>
2020-04-23 16:09:37 -0400

    add deleting single puzzle feature

--- client/components/AllPuzzles.js
+++ client/components/AllPuzzles.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {fetchAllPuzzles} from '../store/puzzles'
+import {fetchAllPuzzles, allPuzzles} from '../store/puzzles'
+import {Link} from 'react-router-dom'
+import axios from 'axios'
 
 export class AllPuzzles extends React.Component {
   constructor(props) {
@@ -11,13 +13,28 @@ export class AllPuzzles extends React.Component {
     this.props.fetchAllPuzzles()
   }
 
+  async removePuzzle(id) {
+    await axios.delete(`api/puzzles/${id}`)
+    this.props.fetchAllPuzzles()
+  }
+
   render() {
     let allPuzzles = this.props.puzzles
+
     return (
       <div>
         {allPuzzles &&
           allPuzzles.map(puzzle => (
             <div key={puzzle.id}>
+              <div>
+                <input
+                  type="button"
+                  value="x"
+                  onClick={this.removePuzzle.bind(this, puzzle.id)}
+                />
+                <Link to={`puzzles/${puzzles.id}`}> {puzzles.name} </Link>
+              </div>
+
               <img src={puzzle.imageUrl} />
               <h3>{puzzle.title}</h3>
               <h3>${puzzle.price / 100}</h3>
