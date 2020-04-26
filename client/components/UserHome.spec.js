11571b122246f7309cd7a479017a3d8e5094e3a6
Carmen <xiawenwu22@gmail.com>
2020-04-25 21:38:59 -0400

    comment out UserHome Spec

--- client/components/UserHome.spec.js
+++ client/components/UserHome.spec.js
@@ -1,13 +1,13 @@
 /* global describe beforeEach it */
 
-import {expect} from 'chai'
-import React from 'react'
-import enzyme, {shallow} from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import {UserHome} from './UserHome'
+// import {expect} from 'chai'
+// import React from 'react'
+// import enzyme, {shallow} from 'enzyme'
+// import Adapter from 'enzyme-adapter-react-16'
+// import {UserHome} from './UserHome'
 
-const adapter = new Adapter()
-enzyme.configure({adapter})
+// const adapter = new Adapter()
+// enzyme.configure({adapter})
 
 // xdescribe('UserHome', () => {
 //   let userHome
