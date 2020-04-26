f978991b6e3a6e406bb06864bbb191fb83aecb69
Amanda Haldy <aehaldy@tutanota.com>
2020-04-25 21:57:45 -0400

    Updates test spec to test home page

--- client/components/user-home.spec.js
+++ client/components/user-home.spec.js
@@ -9,14 +9,14 @@ import {UserHome} from './UserHome'
 const adapter = new Adapter()
 enzyme.configure({adapter})
 
-xdescribe('UserHome', () => {
+describe('UserHome', () => {
   let userHome
 
   beforeEach(() => {
-    userHome = shallow(<UserHome email="cody@email.com" />)
+    userHome = shallow(<UserHome firstName="Thanos" />)
   })
 
-  xit('renders the email in an h3', () => {
-    expect(userHome.find('h3').text()).to.be.equal('Welcome, cody@email.com')
+  it('renders the user name in an h2', () => {
+    expect(userHome.find('h2').text()).to.be.equal('Welcome, Thanos!')
   })
 })
