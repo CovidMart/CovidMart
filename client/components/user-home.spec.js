b1dfbbfbf6f4f57a1e3ae6a0ba797457f1e6dc74
Carmen <xiawenwu22@gmail.com>
2020-04-25 21:53:25 -0400

    modify spec

--- client/components/user-home.spec.js
+++ client/components/user-home.spec.js
@@ -9,14 +9,16 @@ import {UserHome} from './UserHome'
 const adapter = new Adapter()
 enzyme.configure({adapter})
 
-xdescribe('UserHome', () => {
+describe('UserHome', () => {
   let userHome
 
   beforeEach(() => {
-    userHome = shallow(<UserHome email="cody@email.com" />)
+    const singleUser = {email: 'cody@email.com'}
+    // userHome = shallow(<UserHome email="cody@email.com" />)
+    userHome = shallow(<UserHome singleUser={singleUser} />)
   })
 
   it('renders the email in an h3', () => {
-    expect(userHome.find('h3').text()).to.be.equal('Welcome, cody@email.com')
+    expect(userHome.find('h3').text()).to.be.equal('cody@email.com')
   })
 })
